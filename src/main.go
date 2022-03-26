f3068804699725ca1c360d7063db863730b8cbb1
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 20:45:31 -0300

    listo

--- src/main.go
+++ src/main.go
@@ -2,30 +2,27 @@ package main
 
 import "fmt"
 
-func normalFunction(saludo string) {
-	fmt.Println(saludo)
-}
-
-func muchosArgumentos(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func dobleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola Mundo !")
-	muchosArgumentos(1, 10, "hola")
-	value := returnValue(2)
-	fmt.Println("Value: ", value)
-	value1, value2 := dobleReturn(2)
-	fmt.Println("Value1 y Value2", value1, value2)
-	valueOne, _ := dobleReturn(2)
-	fmt.Println("valueOne ", valueOne)
 
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+	// For while
+	fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+	// For forever
+
+	counterForever := 0
+
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
