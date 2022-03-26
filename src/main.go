1cd61e77d41c110388af60720ccb4ebc1c8ee462
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 20:17:20 -0300

    fmt

--- src/main.go
+++ src/main.go
@@ -1,30 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Numeros enteros
-	//int = Depende del OS (32 o 64 bits)
-	//int8 = 8bits = -128 a 127
-	//int16 = 16bits = -2^15 a 2^15-1
-	//int32 = 32bits = -2^31 a 2^31-1
-	//int64 = 64bits = -2^63 a 2^63-1
+	//PrintLn
+	helloMessage := "Hello"
+	worldMessage := "world"
 
-	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
-	//uint = Depende del OS (32 o 64 bits)
-	//uint8 = 8bits = 0 a 127
-	//uint16 = 16bits = 0 a 2^15-1
-	//uint32 = 32bits = 0 a 2^31-1
-	//uint64 = 64bits = 0 a 2^63-1
+	fmt.Println(helloMessage, worldMessage)
 
-	//numeros decimales
-	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
-	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
+	//Printf
+	nombre := "platzi"
+	cursos := 500
+	fmt.Printf("%s tiene mas de %d\n", nombre, cursos)
+	fmt.Printf("%v tiene mas de %v\n", nombre, cursos)
 
-	//textos y booleanos
-	//string = ""
-	//bool = true or false
+	//Sprintf
+	message := fmt.Sprintf("%s tiene mas de %d", nombre, cursos)
+	fmt.Sprintln(message)
 
-	//numeros complejos
-	//Complex64 = Real e Imaginario float32
-	//Complex128 = Real e Imaginario float64
-	//Ejemplo : c:=10 + 8i
+	// Tipo de datos
+	fmt.Printf("helloMessage; %T \n", helloMessage)
+	fmt.Printf("cursos; %T \n", cursos)
 }
