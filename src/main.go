cfff6c4e79952007cb44db258b15f3f5c37ba175
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 20:08:53 -0300

    primitivos

--- src/main.go
+++ src/main.go
@@ -1,40 +1,30 @@
 package main
 
-import "fmt"
-
 func main() {
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("Area cuadrado: ", areaCuadrado)
+	//Numeros enteros
+	//int = Depende del OS (32 o 64 bits)
+	//int8 = 8bits = -128 a 127
+	//int16 = 16bits = -2^15 a 2^15-1
+	//int32 = 32bits = -2^31 a 2^31-1
+	//int64 = 64bits = -2^63 a 2^63-1
 
-	x := 10
-	y := 50
+	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
+	//uint = Depende del OS (32 o 64 bits)
+	//uint8 = 8bits = 0 a 127
+	//uint16 = 16bits = 0 a 2^15-1
+	//uint32 = 32bits = 0 a 2^31-1
+	//uint64 = 64bits = 0 a 2^63-1
 
-	// SUMA
-	result := x + y
-	fmt.Println("Suma:", result)
+	//numeros decimales
+	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
+	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
 
-	// RESTA
-	result = y - x
-	fmt.Println("Resta:", result)
+	//textos y booleanos
+	//string = ""
+	//bool = true or false
 
-	// MULTIPLICACION
-	result = y * x
-	fmt.Println("Multiplicacion:", result)
-
-	// DIVISION
-	result = y / x
-	fmt.Println("Division:", result)
-
-	// MODULO
-	result = y % x
-	fmt.Println("Cuosiente:", result)
-
-	// INCREMENTAL
-	x++
-	fmt.Println("Incrementeal:", x)
-
-	// DECREMENTAL
-	x--
-	fmt.Println("Incrementeal:", x)
+	//numeros complejos
+	//Complex64 = Real e Imaginario float32
+	//Complex128 = Real e Imaginario float64
+	//Ejemplo : c:=10 + 8i
 }
