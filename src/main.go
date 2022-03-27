0cbee5498b61f00c2f4b402c39677b1a1ec6d76d
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 21:19:19 -0300

    listo

--- src/main.go
+++ src/main.go
@@ -1,33 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	var textReverse string
+	text = strings.ToLower(text)
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
 
 func main() {
+	slice := []string{"hola", "que", "hac"}
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
 
-	// ARRAYS (son inmutables)
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
-
-	// SLICES (son mutables)
-
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Metodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append nueva lista
-
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	// Palindormo
+	// amor / roma
+	isPalindromo("Ama")
 }
