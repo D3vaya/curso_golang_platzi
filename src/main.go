f66a59c73017e7e604972ac827cf66ac7ccfbeaf
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 21:08:58 -0300

    listo

--- src/main.go
+++ src/main.go
@@ -4,23 +4,30 @@ import "fmt"
 
 func main() {
 
-	// DEFER
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	// ARRAYS (son inmutables)
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// CONTINUE & BREAK
+	// SLICES (son mutables)
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		if i == 2 {
-			fmt.Println("es 2")
-			continue
-		}
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
+	// Metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-	}
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva lista
+
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
