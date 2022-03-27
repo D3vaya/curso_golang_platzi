d42c18f45b040dbc9688d988c9beb8273de14ca3
Jc Ayala <j.ayala.higuera@gmail.com>
2022-03-26 22:06:10 -0300

    listo

--- src/main.go
+++ src/main.go
@@ -1,15 +1,39 @@
 package main
 
-import (
-	pk "curso_golang_platzi/src/mypackage"
-	"fmt"
-)
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "Pong")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
 
 func main() {
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2022
-	fmt.Println(myCar)
-	pk.PrintMessage("Hola platzi")
+	a := 50
+	b := &a
+	fmt.Println(a)
+	fmt.Println(*b)
 
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPc)
+	myPc.ping()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
 }
